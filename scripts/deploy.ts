4c8b655720ec5c4fb9e9642197b573b2bb3c2223
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-04-07 18:08:13 -0300

    feat: add ballot with time

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+import { formatBytes32String } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Ballot = await ethers.getContractFactory("Ballot");
+  const ballot = await Ballot.deploy([
+    formatBytes32String("Proposal1"),
+    formatBytes32String("Proposal2"),
+  ]);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  await ballot.deployed();
+  console.log("Ballot deployed to:", ballot.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
