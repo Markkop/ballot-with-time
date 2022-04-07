bd09625af15cf3cf0fe9eb6c58119da1c7f32916
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-04-07 18:08:21 -0300

    feat: add ballot time tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,44 @@
 import { expect } from "chai";
+import { formatBytes32String } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { Ballot } from "../typechain";
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  let ballot: Ballot;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  this.beforeEach(async () => {
+    const Ballot = await ethers.getContractFactory("Ballot");
+    ballot = await Ballot.deploy([
+      formatBytes32String("Proposal1"),
+      formatBytes32String("Proposal2"),
+    ]);
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    await ballot.deployed();
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Should vote correctly", async function () {
+    // Arrange
+    const voteTxn = await ballot.vote(1);
+    await voteTxn.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // Act
+    const winningProposal = await ballot.winningProposal();
+
+    // Assert
+    expect(winningProposal).to.equal(1);
+  });
+
+  it("Should not be able to vote after 5 minutes", async function () {
+    // Arrange
+    const blockNum = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNum);
+    const timestamp = block.timestamp;
+    const fiveMinutes = 5 * 60;
+
+    // Act
+    await ethers.provider.send("evm_mine", [timestamp + fiveMinutes + 100]);
+
+    // Assert
+    await expect(ballot.vote(1)).to.be.revertedWith("TooLate()");
   });
 });
